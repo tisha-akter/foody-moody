57fbeb4256edd704826a6075e563e5b7d370459f
tisha-akter <tishaakter.jui79@gmail.com>
2023-11-29 00:30:05 +0600

    .

--- src/components/HomeMenu/HomeMenu.js
+++ src/components/HomeMenu/HomeMenu.js
@@ -6,8 +6,8 @@ export default function HomeMenu() {
             <div className="mx-auto px-4 pt-10">
                 <div className="absolute h-full left-0 right-0 w-full justify-start">
                     <div className="absolute left-0 -top-[100px] text-left -z-10">
-                        <Image
-                            alt="image" src='https://testo-livid.vercel.app/assests/images/img-01.png' width='207' height='296' />
+                        {/* <Image
+                            alt="image" src='https://testo-livid.vercel.app/assests/images/img-01.png' width='207' height='296' /> */}
                     </div>
                     <div>
 
