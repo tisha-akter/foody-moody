7373d12c41740adc4914395fc662eafbed42d1d8
tisha-akter <tishaakter.jui79@gmail.com>
2023-12-05 22:39:50 +0600

    designed resgister page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -36,7 +36,7 @@ const Login = () => {
                                 </div>
                                 <button type="submit" className="w-full text-white bg-orange-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <a href="#" className="font-medium text-orange-600 hover:underline dark:text-primary-500">Sign up</a>
+                                    Don’t have an account yet? <a href="/register" className="font-medium text-orange-600 hover:underline dark:text-primary-500">Sign up</a>
                                 </p>
                             </form>
                         </div>
